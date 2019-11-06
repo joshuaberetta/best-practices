ce89f32a0d5f09979fc172398694e231b9701765
Josh Beretta <joshuaberetta@gmail.com>
2019-11-06 12:50:40 +0200

    adding sum_nums method

--- app/hello_world.py
+++ app/hello_world.py
@@ -1,9 +1,29 @@
 import __future__
 import sys, os
+import pandas as pd
+from typing import List, Any
 
 
-def printing(print_val) -> None:
+def printing(print_val:str) -> None:
+
     if isinstance(print_val, str):
         print(print_val, type(print_val))
     else:
         raise Exception('str type not used')
+
+
+def sum_nums(list_of_nums:List[Any]) -> float:
+
+    sum:float = 0.0
+    for item in list_of_nums:
+        try:
+            item = float(item)
+        except:
+            pass
+        
+        if not isinstance(item, float):
+            raise Exception('list of ints not supplied')
+        else:
+            sum += item
+
+    return sum
--- manage.py
+++ manage.py
@@ -1,9 +1,10 @@
 import __future__
-from app.__main__ import main
+from app.hello_world import printing, sum_nums
 import argparse
 
 parser = argparse.ArgumentParser(description='Hello world! best practices')
 parser.add_argument('--print', default=None)
+parser.add_argument('--sum-nums', default=None, nargs='+')
 args = parser.parse_args()
 
 if args.print is not None:
@@ -12,6 +13,9 @@ if args.print is not None:
         arg = int(arg)
     except:
         pass
-    main(arg)
-else:
-    print('Try printing something to the console using --print "something"')
+    printing(arg)
+
+if args.sum_nums is not None:
+    # print(args.sum_ints)
+    sum_value = sum_nums(args.sum_nums)
+    print(sum_value)
